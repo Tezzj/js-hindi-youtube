db034d851c6057fa0954550ba7cb8e3e173af006
Tejanshu Sharma <146378746+Tezzj@users.noreply.github.com>
2024-08-01 22:38:42 +0530

    Update 04_comparision.js

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -5,17 +5,19 @@
 // console.log(2 != 1);
 
 
-// console.log("2" > 1);
-// console.log("02" > 1);
+// console.log("2" > 1); true
+// console.log("02" > 1); true
 
-console.log(null > 0);
-console.log(null == 0);
+
+// Avoid below types of conversions. Clean code nhi h ye
+console.log(null > 0);    
+console.log(null == 0); // equality check and other comparisons work differently. Other comparisons convert null to 0.
 console.log(null >= 0);
 
 console.log(undefined == 0);
 console.log(undefined > 0);
 console.log(undefined < 0);
 
-// === 
+// ===  double equal(==) checks onlythe values but this checks the datatype also, along with the values.
 
 console.log("2" === 2);
