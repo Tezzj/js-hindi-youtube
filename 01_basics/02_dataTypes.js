0d5269e3e729ae986e8387258ae20ad53929e712
Tejanshu Sharma <146378746+Tezzj@users.noreply.github.com>
2024-08-01 22:08:04 +0530

    Update 02_dataTypes.js

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,29 +1,20 @@
-"use strict"; // treat all JS code as newer version
+"use strict"; // treats all js code as newer version
 
-// alert( 3 + 3) // we are using nodejs, not browser
+//alert(3+3)  // this is node, not browser. So it wont work here
 
-console.log(3 
-    +
-     3) // code readability should be high
+let name = "Tej"  // string
+let age = 22      // number
+let isLogged = false // boolean datatype
 
-console.log("Hitesh")
-
-
-let name = "hitesh"
-let age = 18
-let isLoggedIn = false
-let state;
-
-// number => 2 to power 53
-// bigint
-// string => ""
-// boolean => true/false
-// null => standalone value
-// undefined => 
+// range of number => 2^53
+//bigint -> used for very large numbers
+// null => standalone value(use do define empty(not 0))
+// undefined => used when value is not defined
 // symbol => unique
 
 
 // object
 
+//typeof tells the datatype of the value
 console.log(typeof undefined); // undefined
-console.log(typeof null); // object
+console.log(typeof null); //output m object show hoga
