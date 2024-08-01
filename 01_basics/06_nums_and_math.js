414c440c222515b8eff3d1df9e7a52294939f7c3
Tejanshu Sharma <146378746+Tezzj@users.noreply.github.com>
2024-08-02 03:04:58 +0530

    Update 06_nums_and_math.js

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,30 +1,33 @@
 const score = 400
 // console.log(score);
 
-const balance = new Number(100)
+const balance = new Number(100)  // this creates an object of number type.
 // console.log(balance);
 
-// console.log(balance.toString().length);
+// Every object(created using new keyword) has a prototype which has many functions that can be used on the object.
+// console.log(balance.toString().length);  // the number is converted to string and the string properties can be applied to it.
 // console.log(balance.toFixed(1));
 
 const otherNumber = 123.8966
 
-// console.log(otherNumber.toPrecision(4));
+// console.log(otherNumber.toPrecision(4)); //it returns a STRING which gives a precise value upto the given number of digits, starting from the LEFT of the number, baaki values round off ho jayegi
 
 const hundreds = 1000000
-// console.log(hundreds.toLocaleString('en-IN'));
+// console.log(hundreds.toLocaleString('en-IN'));  // ye comma laga dega numbers k beech m taki achche se samaajh sake
+
+// Number datatype also has some properties attached to it which can also be accessed using dot operator
 
 // +++++++++++++ Maths +++++++++++++++++++++++++++++
 
-// console.log(Math);
-// console.log(Math.abs(-4));
-// console.log(Math.round(4.6));
-// console.log(Math.ceil(4.2));
-// console.log(Math.floor(4.9));
+// console.log(Math);               // Math is also an object which has many properties.
+// console.log(Math.abs(-4));       // changes sign from -ve to +ve
+// console.log(Math.round(4.6));    // to round off
+// console.log(Math.ceil(4.2));     // gives next integer
+// console.log(Math.floor(4.9));    // gives previous integer
 // console.log(Math.min(4, 3, 6, 8));
 // console.log(Math.max(4, 3, 6, 8));
 
-console.log(Math.random());
+console.log(Math.random());    // will give a random value between 0 and 1
 console.log((Math.random()*10) + 1);
 console.log(Math.floor(Math.random()*10) + 1);
 
