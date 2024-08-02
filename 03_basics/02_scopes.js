23381871b578b0706cb692cc82baf90fead00826
Tejanshu Sharma <146378746+Tezzj@users.noreply.github.com>
2024-08-02 23:58:36 +0530

    Update 02_scopes.js

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,15 +1,15 @@
 //var c = 300
-let a = 300
-if (true) {
+//let a = 300 
+if (true) {         // any variable inside the curly braces has a scope limited in that block only. You can't access that variable outside it's scope. It is called local variable.
     let a = 10
     const b = 20
     // console.log("INNER: ", a);
-    
+    //var c = 30   // The value of var c will remain 30 even outside of this scope, even if it has another value outside of the scope. That's why don't use var
 }
 
 
 
-// console.log(a);
+ console.log(a);
 // console.log(b);
 // console.log(c);
 
@@ -44,7 +44,7 @@ if (true) {
 // ++++++++++++++++++ interesting ++++++++++++++++++
 
 
-console.log(addone(5))
+console.log(addone(5))    // This will get executed. As the function is declared separately
 
 function addone(num){
     return num + 1
@@ -52,7 +52,7 @@ function addone(num){
 
 
 
-addTwo(5)
+addTwo(5)                         // This will throw an error as addtwo is being accessed before its initialisation. Isko declare krne k saath saath variable m hold bhi kr diya
 const addTwo = function(num){
     return num + 2
 }
