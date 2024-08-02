bebff6a0eff6a43ad71ce12e445fd440c3b4017e
Tejanshu Sharma <146378746+Tezzj@users.noreply.github.com>
2024-08-03 02:09:38 +0530

    Update 04_iife.js

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,10 +1,10 @@
 // Immediately Invoked Function Expressions (IIFE)
-
+// Sometimes, we can have problem from pollution of global scope, that is, tehy can interfere with the function. So, to prevent it, and invoke functions immediately, we use iffe
 
 (function chai(){
     // named IIFE
     console.log(`DB CONNECTED`);
-})();
+})();                              //semi colon is necessary to stop the function as the function, once invoked, doesn't stop as it doesn't know the context to stop
 
 ( (name) => {
     console.log(`DB CONNECTED TWO ${name}`);
