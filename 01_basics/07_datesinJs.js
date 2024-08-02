8a7ff284ad2dc557bda674261a0e94b754c49438
Tejanshu Sharma <146378746+Tezzj@users.noreply.github.com>
2024-08-02 15:51:54 +0530

    Update 07_datesinJs.js

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -1,18 +1,18 @@
 // Dates
 
-let myDate = new Date()
-// console.log(myDate.toString());
+let myDate = new Date()                       // Date is also an object with many methods
+// console.log(myDate.toString());            // toString is an in built function which can be used for any object
 // console.log(myDate.toDateString());
 // console.log(myDate.toLocaleString());
 // console.log(typeof myDate);
 
 // let myCreatedDate = new Date(2023, 0, 23)
 // let myCreatedDate = new Date(2023, 0, 23, 5, 3)
-// let myCreatedDate = new Date("2023-01-14")
+// let myCreatedDate = new Date("2023-01-14")               // when we input in the form of dd-mm-yy, then the month count starts from 1
 let myCreatedDate = new Date("01-14-2023")
-// console.log(myCreatedDate.toLocaleString());
+// console.log(myCreatedDate.toLocaleString()); 
 
-let myTimeStamp = Date.now()
+let myTimeStamp = Date.now()                        // gives the time passed from jan 1, 1970 till now in miliseconds
 
 // console.log(myTimeStamp);
 // console.log(myCreatedDate.getTime());
@@ -20,12 +20,12 @@ let myTimeStamp = Date.now()
 
 let newDate = new Date()
 console.log(newDate);
-console.log(newDate.getMonth() + 1);
-console.log(newDate.getDay());
+console.log(newDate.getMonth() + 1);         // Month starts from 0
+console.log(newDate.getDay());               // Day starts from 1 starting from monday
 
 // `${newDate.getDay()} and the time `
 
-newDate.toLocaleString('default', {
+newDate.toLocaleString('default', {                     // In this method, we can define an object also
     weekday: "long",
     
 })
