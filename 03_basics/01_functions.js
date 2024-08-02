33db95185dc6c8fd80b7a2c0896ee8a232dd9c1a
Tejanshu Sharma <146378746+Tezzj@users.noreply.github.com>
2024-08-02 21:11:22 +0530

    Update 01_functions.js

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -8,26 +8,27 @@ function sayMyName(){
     console.log("H");
 }
 
-// sayMyName()
+// sayMyName()    //This executes the function
+// sayMyName      // This gives the reference of the function. It wont output anything
 
 // function addTwoNumbers(number1, number2){
 
 //     console.log(number1 + number2);
 // }
 
-function addTwoNumbers(number1, number2){
+function addTwoNumbers(number1, number2){   // number1 and number2 are the parameters which accept the arguments
 
     // let result = number1 + number2
     // return result
     return number1 + number2
 }
 
-const result = addTwoNumbers(3, 5)
+const result = addTwoNumbers(3, 5)       // 3, 5 are arguments which are passed to the parameters of the function
 
 // console.log("Result: ", result);
 
 
-function loginUserMessage(username = "sam"){
+function loginUserMessage(username = "sam"){          // username is a default parameter, just like in c++
     if(!username){
         console.log("PLease enter a username");
         return
@@ -39,11 +40,13 @@ function loginUserMessage(username = "sam"){
 // console.log(loginUserMessage("hitesh"))
 
 
-function calculateCartPrice(val1, val2, ...num1){
+function calculateCartPrice(val1, val2, ...num1){        
+// ... is a rest operator(it is also spread operator, but use depends upon the situation). We can pass multiple arguments in num1 and it will give an array of them.
     return num1
 }
 
-// console.log(calculateCartPrice(200, 400, 500, 2000))
+// console.log(calculateCartPrice(200, 400, 500, 2000)) 
+//output is [500, 2000] as first two arguments will go to val1 and val2
 
 const user = {
     username: "hitesh",
@@ -55,7 +58,7 @@ function handleObject(anyobject){
 }
 
 // handleObject(user)
-handleObject({
+handleObject({               // We can also directly pass an object into the function argument
     username: "sam",
     price: 399
 })
