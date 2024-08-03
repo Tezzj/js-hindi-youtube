bed620cd499372161c57012cde60364aacd34487
Tejanshu Sharma <146378746+Tezzj@users.noreply.github.com>
2024-08-03 17:59:19 +0530

    Update three.js

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -5,6 +5,8 @@
 
 const arr = [1, 2, 3, 4, 5]
 
+
+// syntax: for(const iterator of object){}  ye wala object alag h, js wala object nhi h
 for (const num of arr) {
     //console.log(num);
 }
@@ -13,17 +15,35 @@ const greetings = "Hello world!"
 for (const greet of greetings) {
     //console.log(`Each char is ${greet}`)
 }
+/* output:
+Each char is H
+Each char is e
+Each char is l
+Each char is l
+Each char is o
+Each char is 
+Each char is w
+Each char is o
+Each char is r
+Each char is l
+Each char is d
+Each char is !
+*/
 
 // Maps
 
-const map = new Map()
+const map = new Map()       //Map is an object with many mmethods. It also holds the key value pairs and remembers the original insertion order of keys 
 map.set('IN', "India")
 map.set('USA', "United States of America")
 map.set('Fr', "France")
-map.set('IN', "India")
+map.set('IN', "India")   // map will not store this as map stores unique values.
 
 
 // console.log(map);
+
+//for (const [key, value] of map) {
+    // console.log(key)     It will give array of key and value pair, for each of them
+//}    
 
 for (const [key, value] of map) {
     // console.log(key, ':-', value);
@@ -34,7 +54,7 @@ const myObject = {
     game2: 'Spiderman'
 }
 
-// for (const [key, value] of myObject) {
+// for (const [key, value] of myObject) {    // This will not work as for of loop doesn't work on object, but works on map
 //     console.log(key, ':-', value);
     
 // }
