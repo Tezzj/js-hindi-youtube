188cdb534226f15bbf86bd5c6f5693dfc07b69a3
Tejanshu Sharma <146378746+Tezzj@users.noreply.github.com>
2024-08-04 10:58:13 +0530

    Update and rename nine.js to nine_reduce.js

--- 05_iterations/nine_reduce.js
+++ 05_iterations/nine_reduce.js
@@ -1,15 +1,16 @@
 const myNums = [1, 2, 3]
 
-// const myTotal = myNums.reduce(function (acc, currval) {
-//     console.log(`acc: ${acc} and currval: ${currval}`);
+// const myTotal = myNums.reduce(function (acc, currval) {       // this function gives the total sum of all the values in an array
+//     console.log(`acc: ${acc} and currval: ${currval}`);       // The value of accumulator is the returned sum of current + accumulator
 //     return acc + currval
-// }, 0)
+// }, 0) // 0 is igven as an initial value that accumulator takes.
 
 const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
 
 console.log(myTotal);
 
 
+// this is a shopping cart which is an array of objects. We have applied reduce method here, to get the total sum of the price
 const shoppingCart = [
     {
         itemName: "js course",
