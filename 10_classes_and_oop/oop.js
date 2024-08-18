b53e6f1863955683071e0afc2169d67b97fa8192
Tejanshu Sharma <146378746+Tezzj@users.noreply.github.com>
2024-08-18 20:58:10 +0530

    Update oop.js

--- 10_classes_and_oop/oop.js
+++ 10_classes_and_oop/oop.js
@@ -28,10 +28,36 @@ function User(username, loginCount, isLoggedIn){
 
     }
 
-    return this
+    return this          // It will return the current object. It is implicitly defined and we don't need to write this
 }
 
-const userOne = new User("hitesh", 12, true)
+/*
+const userOne = User("hitesh", 12, true)
+console.log(userOne)
+OUTPUT:
+    username = 'hitesh'
+    loginCount = 12
+    isLoggedIn = true
+    
+const userTwo = User("ChaiAurCode", 11, false)  // If we do not use new, then it will overwrite the userOne object
+console.log(userOne)
+OUTPUT:
+    username = 'ChaiAurCode'
+    loginCount = 11
+    isLoggedIn = false
+
+*/
+
+/*
+When new keyword is used:
+Step 1: A new and empty object(instance) is created
+Step 2: A constructor function is called because of the new keyword. And the arguments are given to it
+Step 3: Arguments are injected into this keyword(gives current context of the new object)
+Step 4: They are in the function
+
+*/
+
+const userOne = new User("hitesh", 12, true)        // new is a constructor function which will give a new instance(new copy) 
 const userTwo = new User("ChaiAurCode", 11, false)
-console.log(userOne.constructor);
+console.log(userOne.constructor);     //constructor property(about an object or function) gives the reference about itself
 //console.log(userTwo);
