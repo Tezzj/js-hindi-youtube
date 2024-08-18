b0fbad73e73511bbbe53a26c5119212232c9d035
Tejanshu Sharma <146378746+Tezzj@users.noreply.github.com>
2024-08-18 21:27:43 +0530

    Update Object.js

--- 10_classes_and_oop/Object.js
+++ 10_classes_and_oop/Object.js
@@ -1,13 +1,16 @@
-function multipleBy5(num){
+function multipleBy5(num){  
 
     return num*5
 }
 
-multipleBy5.power = 2
+multipleBy5.power = 2     // we can make a function behave like an object also as function is also an object in a way(prototypal inheritance)
 
 console.log(multipleBy5(5));
 console.log(multipleBy5.power);
-console.log(multipleBy5.prototype);
+
+console.log(multipleBy5.prototype);   
+// It gives an empty paranthesis {}. It stores the context of a property(prototype has internal methods and propeties for a function also).
+// If 'this' is used in a funtion, it's context will be there in the prototype.
 
 function createUser(username, score){
     this.username = username
@@ -15,16 +18,19 @@ function createUser(username, score){
 }
 
 createUser.prototype.increment = function(){
-    this.score++
+    this.score++        // 'this' means jis. It gives the current context of the function that calls it.
 }
-createUser.prototype.printMe = function(){
+createUser.prototype.printMe = function(){       // we can add some properties defined by us in the prototype of the object
     console.log(`price is ${this.score}`);
 }
+
+ 
 
 const chai = new createUser("chai", 25)
 const tea = createUser("tea", 250)
 
-chai.printMe()
+chai.printMe()     // If we use this without using new keyword, then it will give error that printMe is undefined. So we have to crate a new instance to transfer
+                   // the properyies to the variable(chai in this case)
 
 
 /*
@@ -33,10 +39,13 @@ Here's what happens behind the scenes when the new keyword is used:
 
 A new object is created: The new keyword initiates the creation of a new JavaScript object.
 
-A prototype is linked: The newly created object gets linked to the prototype property of the constructor function. This means that it has access to properties and methods defined on the constructor's prototype.
+A prototype is linked: The newly created object gets linked to the prototype property of the constructor function. This means that it has access to
+properties and methods defined on the constructor's prototype.
 
-The constructor is called: The constructor function is called with the specified arguments and this is bound to the newly created object. If no explicit return value is specified from the constructor, JavaScript assumes this, the newly created object, to be the intended return value.
+The constructor is called: The constructor function is called with the specified arguments and this is bound to the newly created object. If no explicit
+return value is specified from the constructor, JavaScript assumes this, the newly created object, to be the intended return value.
 
-The new object is returned: After the constructor function has been called, if it doesn't return a non-primitive value (object, array, function, etc.), the newly created object is returned.
+The new object is returned: After the constructor function has been called, if it doesn't return a non-primitive value
+(object, array, function, etc.), the newly created object is returned.
 
 */
