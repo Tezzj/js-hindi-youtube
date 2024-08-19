53c0f70fbf807599da0c5eb7ed776210b4897002
Tejanshu Sharma <146378746+Tezzj@users.noreply.github.com>
2024-08-20 02:45:14 +0530

    Update inheritance.js

--- 10_classes_and_oop/inheritance.js
+++ 10_classes_and_oop/inheritance.js
@@ -8,9 +8,10 @@ class User {
     }
 }
 
-class Teacher extends User{
+class Teacher extends User{                          // similar to .toPrototype() method.   // It means Teacher inherits user class
     constructor(username, email, password){
-        super(username)
+        super(username)   
+// super keyword passes the parameter to the parent class of the inherited class and does all the work of getting the current context, just like call would.
         this.email = email
         this.password = password
     }
@@ -20,11 +21,11 @@ class Teacher extends User{
     }
 }
 
-const chai = new Teacher("chai", "chai@teacher.com", "123")
+const chai = new Teacher("chai", "chai@teacher.com", "123")     // Teacher constructor cannot be invoked without new keyword.
 
 chai.logMe()
 const masalaChai = new User("masalaChai")
 
 masalaChai.logMe()
 
-console.log(chai instanceof User);
+console.log(chai instanceof User);     // used to check the instance of a class or object. Here, Teacher is an instance of the User
