dc9884efb8b0b55936aed442db7b0dc11c4021c7
Tejanshu Sharma <146378746+Tezzj@users.noreply.github.com>
2024-08-20 20:59:39 +0530

    Update mathpi.js

--- 10_classes_and_oop/mathpi.js
+++ 10_classes_and_oop/mathpi.js
@@ -1,10 +1,33 @@
-const descripter = Object.getOwnPropertyDescriptor(Math, "PI")
+const descripter = Object.getOwnPropertyDescriptor(Math, "PI") 
+/* 
+
+The Object.getOwnPropertyDescriptor() method in JavaScript is used to retrieve the descriptor object for a property on a given object.
+The descriptor object provides details about that property, such as whether it is writable, enumerable, and configurable.
+
+This method returns a property descriptor for the given property of the specified object.
+
+It takes two arguments:
+The object you want to inspect (Math in this case).
+The name of the property you want to get the descriptor for ("PI" in this case).
+
+*/
 
 // console.log(descripter);
+/*
+OUTPUT:
+{
+  value: 3.141592653589793,
+  writable: false,
+  enumerable: false,
+  configurable: false
+}
+
+*/
 
 // console.log(Math.PI);
-// Math.PI = 5
-// console.log(Math.PI);
+// Math.PI = 5               // We cannot change the value of PI as it's property is hard-coded to prevent it from getting changed
+// console.log(Math.PI);          
+
 
 const chai = {
     name: 'ginger chai',
@@ -18,17 +41,18 @@ const chai = {
 
 console.log(Object.getOwnPropertyDescriptor(chai, "name"));
 
-Object.defineProperty(chai, 'name', {
+Object.defineProperty(chai, 'name', {     // We can change the properties of user-created objects.
     //writable: false,
+    //enumerable: false    // This will not allow name to be iterated in the loop.
     enumerable: true,
     
 })
 
 console.log(Object.getOwnPropertyDescriptor(chai, "name"));
 
-for (let [key, value] of Object.entries(chai)) {
+for (let [key, value] of Object.entries(chai)) {    // Iterated through the object
+    //Object.entries(chai) would return: [["username", "chai"], ["score", 25]]
     if (typeof value !== 'function') {
-        
         console.log(`${key} : ${value}`);
     }
 }
